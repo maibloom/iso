3761ee96bbf582e9025517bcf8e07b53fff863db
amirali-dashti <damirali298@gmail.com>
2025-03-23 14:42:10 +0330

    modified:   bloom-nix/hosts/iso/default.nix

--- bloom-nix/hosts/iso/default.nix
+++ bloom-nix/hosts/iso/default.nix
@@ -1,111 +1,337 @@
-# ISO-specific configuration for Bloom Nix - Flake compatible
-{ config, lib, pkgs, inputs, outputs, ... }:
+# modules/calamares/default.nix
+# This module configures Calamares installer for Bloom Nix
+{ config, lib, pkgs, ... }:
 
 {
-  # ISO-specific configuration
-  isoImage = {
-    # Set ISO filename and volume ID
-    isoName = lib.mkForce "bloom-nix.iso";
-    volumeID = lib.mkForce "BLOOM_NIX";
-
-    # Make the ISO bootable via both BIOS and UEFI
-    makeEfiBootable = true;
-    makeUsbBootable = true;
-
-    # Add build information to the ISO label
-    appendToMenuLabel = " Live";
-    squashfsCompression = "gzip";
-  };
-
-  # Live environment user configuration
-  users.users.nixos = {
-    isNormalUser = true;
-    description = "Bloom Nix Live User";
-    extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
-    # No password for live environment
-    initialPassword = "";
-  };
-
-  # Allow sudo without password for live environment
-  security.sudo.wheelNeedsPassword = false;
-
-  # Desktop shortcuts - create these in the appropriate location
-  environment.etc = {
-    "skel/Desktop/install.desktop".text = ''
-      [Desktop Entry]
-      Type=Application
-      Name=Install Bloom Nix
-      Comment=Install the operating system to your computer
-      Exec=calamares
-      Icon=calamares
-      Terminal=false
-      Categories=System;
-    '';
-    
-    "skel/Desktop/terminal.desktop".text = ''
-      [Desktop Entry]
-      Type=Application
-      Name=Terminal
-      Comment=Access the command line
-      Exec=${lib.getBin pkgs.libsForQt5.konsole}/bin/konsole
-      Icon=utilities-terminal
-      Terminal=false
-      Categories=System;
-    '';
-  };
-
-  # Add disk utilities for the installation process
-  environment.systemPackages = with pkgs; [
-    # Disk utilities needed by the installer
-    gparted
-    parted
-    ntfs3g
-    dosfstools
-    e2fsprogs
-    btrfs-progs
-    xfsprogs
-    cryptsetup
-    lvm2
-    
-    # Other useful utilities
-    firefox
-    git
-    wget
-    curl
-    htop
+  # Import all Calamares module configurations
+  imports = [
+    ./packagechooser.nix
   ];
 
-  # Automatically log in the live user
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "nixos";
+  # Install Calamares
+  environment.systemPackages = with pkgs; [
+    pkgs.calamares
+    # These dependencies are needed for Calamares to function properly
+    parted
+    gptfdisk
+    cryptsetup
+    dmidecode
+  ];
+
+  # Basic Calamares configuration
+  environment.etc = {
+    # Main Calamares settings
+    "calamares/settings.conf".text = ''
+      # Configuration file for Calamares
+      # Execution sequence for Bloom Nix installer
+      ---
+      sequence:
+        - show:
+            - welcome
+            - locale
+            - keyboard
+            - partition
+            - users
+            - summary
+        - exec:
+            - partition
+            - mount
+            - unpackfs
+            - networkcfg
+            - machineid
+            - fstab
+            - locale
+            - keyboard
+            - localecfg
+            - users
+            - displaymanager
+            - packagechooser
+            - packages
+            - grubcfg
+            - bootloader
+            - umount
+        - show:
+            - finished
+
+      # Branding settings
+      branding: bloom
+      
+      # Prompt when quitting
+      prompt-install: true
+      
+      # Don't show "Cancel" button
+      dont-chroot: false
+      
+      # Allow canceling the installation
+      disable-cancel: false
+      
+      # Allow quitting the installer
+      disable-cancel-during-exec: false
+      quit-at-end: false
+    '';
+
+    # Welcome module configuration
+    "calamares/modules/welcome.conf".text = ''
+      # Welcome module configuration for Calamares
+      ---
+      showSupportUrl:         true
+      showKnownIssuesUrl:     true
+      showReleaseNotesUrl:    true
+      
+      # Requirements checking
+      requireStorage:         true
+      requireMemory:          true
+      requireInternet:        false
+      requireSwap:            false
+      
+      # Set minimum values
+      requiredStorage:        20
+      requiredRam:            2.0
+      
+      # URLs
+      supportUrl:             "https://github.com/yourusername/bloom-nix/issues"
+      knownIssuesUrl:         "https://github.com/yourusername/bloom-nix/issues"
+      releaseNotesUrl:        "https://github.com/yourusername/bloom-nix/releases"
+      
+      # Check for internet connection
+      internetCheckUrl:       "https://1.1.1.1"
+    '';
+
+    # Users module configuration
+    "calamares/modules/users.conf".text = ''
+      # Users module configuration for Calamares
+      ---
+      # Default username to be shown
+      defaultUserName:                "user"
+      
+      # Default full name
+      defaultFullName:                "Bloom Nix User"
+      
+      # Default password for the user
+      defaultPassword:                ""
+      
+      # Default hostname
+      defaultHostName:                "bloom-nix"
+      
+      # What login shells to offer
+      loginShells:
+        - name: "Bash"
+          command: "/bin/bash"
+          shortcut: "b"
+        - name: "Zsh"
+          command: "/bin/zsh"
+          shortcut: "z"
+      
+      # Default shell to use
+      defaultLoginShell:              "/bin/bash"
+      
+      # Require password strength
+      requireStrongPasswords:         true
+      
+      # Auto-login user
+      doAutoLogin:                    true
+      
+      # Set root password the same as user password
+      setRootPassword:                false
+      
+      # Allow to select the root account name independently
+      allowRootUser:                  false
+      
+      # Password checking
+      passwordStrengthWarningLevel:   50
+      
+      # Allow non-ASCII names
+      allowWeakPasswords:             false
+      
+      # Only show username and hostname fields (for preconfigured setups)
+      allowWeakPasswordsDefault:      false
+    '';
+
+    # Packages module configuration (works with packagechooser)
+    "calamares/modules/packages.conf".text = ''
+      # Packages module configuration for Calamares
+      ---
+      # Manage system packages during installation
+      # Update the system, install/remove packages
+      #
+      # The field 'operations' is a list of operations to perform
+      # during installation:
+      # - the install target package
+      # - the remove target package
+      # - the try_install target package (if possible, not critical)
+      # - the try_remove target package (if possible, not critical)
+      # 
+      # Each operation can have an empty list of packages, in which case
+      # nothing is done for that operation.
+      operations:
+        - install:
+            - sudo
+            - networkmanager
+            - plasma-desktop
+            - firefox
+            - konsole
+            - dolphin
+            - kate
+        - remove:
+            - calamares-framework
+      
+      # Package manager to use - for NixOS we don't really use this directly,
+      # but need to specify something for Calamares to be happy
+      backend: apt
+      
+      # Skip the installation if the system doesn't need it
+      skip_if_no_internet: false
+      
+      # Don't update the package database
+      update_db: false
+      
+      # Don't update system packages
+      update_system: false
+    '';
+
+    # Unpackfs module configuration 
+    "calamares/modules/unpackfs.conf".text = ''
+      # Unpackfs module configuration for Calamares
+      ---
+      # Unpack the rootfs to the destination
+      unpack:
+        -   source: "/run/media/nix-store"
+            sourcefs: "squashfs"
+            destination: ""
+    '';
+
+    # Create branding directory
+    "calamares/branding/bloom/branding.desc".text = ''
+      ---
+      componentName:   bloom
+      welcomeStyleCalamares: true
+      welcomeExpandingLogo: true
+      
+      strings:
+          productName:         Bloom NixOS
+          shortProductName:    Bloom Nix
+          version:             1.0.0
+          shortVersion:        1.0
+          versionedName:       Bloom NixOS 1.0.0
+          shortVersionedName:  Bloom 1.0
+          bootloaderEntryName: Bloom NixOS
+          productUrl:          https://github.com/yourusername/bloom-nix
+          supportUrl:          https://github.com/yourusername/bloom-nix/issues
+          
+      images:
+          productLogo:         "logo.png"
+          productIcon:         "logo.png"
+          productWelcome:      "welcome.png"
+          
+      slideshow:               "show.qml"
+      
+      style:
+          sidebarBackground:   "#2D2D2D"
+          sidebarText:         "#FFFFFF"
+          sidebarTextSelect:   "#FFFFFF"
+          sidebarTextHighlight: "#74C0E3"
+    '';
+
+    # Create a simple slideshow for the installer
+    "calamares/branding/bloom/show.qml".text = ''
+      /* Slideshow QML file for Calamares */
+      
+      import QtQuick 2.0;
+      import calamares.slideshow 1.0;
+      
+      Presentation
+      {
+          id: presentation
+          
+          Timer {
+              interval: 20000
+              running: true
+              repeat: true
+              onTriggered: presentation.goToNextSlide()
+          }
+          
+          Slide {
+              Image {
+                  id: background1
+                  source: "slide1.png"
+                  anchors.centerIn: parent
+                  width: parent.width
+                  height: parent.height
+                  horizontalAlignment: Image.AlignCenter
+                  verticalAlignment: Image.AlignCenter
+                  
+                  Text {
+                      anchors.centerIn: parent
+                      text: "Welcome to Bloom NixOS"
+                      color: "white"
+                      font.pixelSize: 32
+                      font.bold: true
+                  }
+              }
+          }
+          
+          Slide {
+              Image {
+                  id: background2
+                  source: "slide2.png"
+                  anchors.centerIn: parent
+                  width: parent.width
+                  height: parent.height
+                  horizontalAlignment: Image.AlignCenter
+                  verticalAlignment: Image.AlignCenter
+                  
+                  Text {
+                      anchors.centerIn: parent
+                      text: "Reproducible and Declarative System Configuration"
+                      color: "white"
+                      font.pixelSize: 32
+                      font.bold: true
+                  }
+              }
+          }
+          
+          Slide {
+              Image {
+                  id: background3
+                  source: "slide3.png"
+                  anchors.centerIn: parent
+                  width: parent.width
+                  height: parent.height
+                  horizontalAlignment: Image.AlignCenter
+                  verticalAlignment: Image.AlignCenter
+                  
+                  Text {
+                      anchors.centerIn: parent
+                      text: "Rolling Updates and Atomic Upgrades"
+                      color: "white"
+                      font.pixelSize: 32
+                      font.bold: true
+                  }
+              }
+          }
+      }
+    '';
   };
 
-  # Enable the needed polkit rules for disk mounting and system installation
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if (
-        subject.isInGroup("wheel") &&
-        (
-          action.id.indexOf("org.freedesktop.udisks2.") == 0 ||
-          action.id.indexOf("org.freedesktop.login1.") == 0 ||
-          action.id.indexOf("org.freedesktop.systemd1.") == 0
-        )
-      ) {
-        return polkit.Result.YES;
-      }
-    });
-  '';
-  
-  # Create an autostart entry for Calamares installer reminder
-  environment.etc."xdg/autostart/calamares-reminder.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Name=Bloom Installer Reminder
-    Exec=sh -c 'zenity --info --title="Bloom NixOS" --text="Welcome to Bloom NixOS!\n\nYou can install the system by clicking on the installer icon on the desktop." --icon-name=system-software-install'
-    Icon=system-software-install
-    Terminal=false
-    StartupNotify=true
-    X-KDE-autostart-phase=application
+  # Create necessary directories and symlinks
+  system.activationScripts.calamaresSetup = ''
+    mkdir -p /etc/calamares/branding/bloom
+    mkdir -p /etc/calamares/images
+    mkdir -p /etc/calamares/modules
+    
+    # Create symlinks for branding assets if they don't exist yet
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/branding/bloom/logo.png
+    ln -sf ${../../modules/branding/assets/grub-background.png} /etc/calamares/branding/bloom/slide1.png
+    ln -sf ${../../modules/branding/assets/grub-background.png} /etc/calamares/branding/bloom/slide2.png
+    ln -sf ${../../modules/branding/assets/grub-background.png} /etc/calamares/branding/bloom/slide3.png
+    ln -sf ${../../modules/branding/assets/grub-background.png} /etc/calamares/branding/bloom/welcome.png
+    
+    # Create symlinks for package selection category images
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/images/base.png
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/images/dev.png
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/images/gaming.png
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/images/multimedia.png
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/images/office.png
+    ln -sf ${../../modules/branding/assets/logo.png} /etc/calamares/images/science.png
   '';
 }
