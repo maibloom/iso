deddfc736d8bbf9a4faac8e080f96ee451631a61
amirali-dashti <damirali298@gmail.com>
2025-03-26 01:17:11 +0330

    modified:   main/flake.nix

--- main/flake.nix
+++ main/flake.nix
@@ -1,42 +1,70 @@
 {
-  description = "A minimal NixOS ISO configuration";
+  description = "My minimal ISO configuration";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # or your desired branch
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.minimal-iso = nixpkgs.lib.nixos.mkNixosConfig {
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
-        ./modules/base/default.nix
-        ./modules/hardware/default.nix
-        # vmSupportModule
-        {
-          services.xserver.enable = true;
-          services.xserver.desktopManager.xfce.enable = true;
-          services.xserver.displayManager.lightdm.enable = true;
-          services.xserver.displayManager.autoLogin = {
-            enable = true;
-            user = "nixos";
-          };
-          users.users.nixos = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" "networkmanager" "video" ];
-            initialPassword = "";
-          };
-          security.sudo.wheelNeedsPassword = false;
-          environment.systemPackages = with pkgs; [
-            firefox
-            xfce.xfce4-terminal
-            gparted
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        "minimal-iso" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            # ISO image creation module from nixpkgs
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+
+            # Essential modules
+            ./modules/base/default.nix
+            ./modules/hardware/default.nix
+
+            # VM support (ensure vmSupportModule is defined or imported)
+            vmSupportModule
+
+            # Minimal desktop configuration
+            {
+              services.xserver.enable = true;
+
+              # Lightweight desktop environment: XFCE
+              services.xserver.desktopManager.xfce.enable = true;
+              services.xserver.displayManager.lightdm.enable = true;
+
+              # Auto-login settings
+              services.xserver.displayManager.autoLogin = {
+                enable = true;
+                user = "nixos";
+              };
+
+              # User configuration
+              users.users.nixos = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" "networkmanager" "video" ];
+                initialPassword = "";
+              };
+
+              # Sudo settings
+              security.sudo.wheelNeedsPassword = false;
+
+              # Basic packages
+              environment.systemPackages = with nixpkgs.pkgs; [
+                firefox
+                xfce.xfce4-terminal
+                gparted
+              ];
+
+              # ISO image settings
+              isoImage = {
+                edition = "minimal";
+                isoName = "bloom-minimal.iso";
+                makeEfiBootable = true;
+                makeUsbBootable = true;
+              };
+            }
           ];
-          isoImage = {
-            edition = "minimal";
-            isoName = "bloom-minimal.iso";
-            makeEfiBootable = true;
-            makeUsbBootable = true;
-          };
-        }
-      ];
+        };
+      };
     };
-  };
 }
+
+
+
