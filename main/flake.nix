2a6cfa2dad4f0fedb60d892b0a73ba87a69bb08d
amirali-dashti <damirali298@gmail.com>
2025-03-26 01:20:39 +0330

    new file:   main/flake.lock 	modified:   main/flake.nix

--- main/flake.nix
+++ main/flake.nix
@@ -1,70 +1,73 @@
 {
-  description = "My minimal ISO configuration";
+  description = "Minimal ISO configuration with an ISO output alias";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      minimalIso = pkgs.lib.nixosSystem {
+        system = system;
+        modules = [
+          # ISO image creation module from nixpkgs
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+          # Essential modules
+          ./modules/base/default.nix
+          ./modules/hardware/default.nix
+          # VM support (make sure vmSupportModule is defined or imported)
+          vmSupportModule
+          # Minimal desktop configuration
+          {
+            services.xserver.enable = true;
+            services.xserver.desktopManager.xfce.enable = true;
+            services.xserver.displayManager.lightdm.enable = true;
+            services.xserver.displayManager.autoLogin = {
+              enable = true;
+              user = "nixos";
+            };
+            users.users.nixos = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" "networkmanager" "video" ];
+              initialPassword = "";
+            };
+            security.sudo.wheelNeedsPassword = false;
+            environment.systemPackages = with pkgs; [
+              firefox
+              xfce.xfce4-terminal
+              gparted
+            ];
+            isoImage = {
+              edition = "minimal";
+              isoName = "bloom-minimal.iso";
+              makeEfiBootable = true;
+              makeUsbBootable = true;
+            };
+          }
+        ];
+      };
     in {
+      # Your NixOS configuration output:
       nixosConfigurations = {
-        "minimal-iso" = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            # ISO image creation module from nixpkgs
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+        minimal-iso = minimalIso;
+      };
 
-            # Essential modules
-            ./modules/base/default.nix
-            ./modules/hardware/default.nix
+      # Expose the ISO image directly as a top-level output:
+      iso = minimalIso.config.system.build.isoImage;
 
-            # VM support (ensure vmSupportModule is defined or imported)
-            # vmSupportModule
-
-            # Minimal desktop configuration
-            {
-              services.xserver.enable = true;
-
-              # Lightweight desktop environment: XFCE
-              services.xserver.desktopManager.xfce.enable = true;
-              services.xserver.displayManager.lightdm.enable = true;
-
-              # Auto-login settings
-              services.xserver.displayManager.autoLogin = {
-                enable = true;
-                user = "nixos";
-              };
-
-              # User configuration
-              users.users.nixos = {
-                isNormalUser = true;
-                extraGroups = [ "wheel" "networkmanager" "video" ];
-                initialPassword = "";
-              };
-
-              # Sudo settings
-              security.sudo.wheelNeedsPassword = false;
-
-              # Basic packages
-              environment.systemPackages = with nixpkgs.pkgs; [
-                firefox
-                xfce.xfce4-terminal
-                gparted
-              ];
-
-              # ISO image settings
-              isoImage = {
-                edition = "minimal";
-                isoName = "bloom-minimal.iso";
-                makeEfiBootable = true;
-                makeUsbBootable = true;
-              };
-            }
-          ];
+      # (Optionally) Also add the ISO under packages and legacyPackages:
+      packages = {
+        "${system}" = {
+          iso = minimalIso.config.system.build.isoImage;
+        };
+      };
+      legacyPackages = {
+        "${system}" = {
+          iso = minimalIso.config.system.build.isoImage;
         };
       };
     };
 }
-
-
-
