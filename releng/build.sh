a9fbe90be9ed9f1645a4d0ec7fc4b727fb6d7ef9
amiralidashti <damirali298@gmail.com>
2025-06-01 22:00:49 +0330

    modified:   releng/build.sh

--- releng/build.sh
+++ releng/build.sh
@@ -4,12 +4,12 @@
 # Clean duplicate entries
 #sort -u packages.x86_64 -o packages.x86_64
 
-sudo mkdir ~/maibloom-temp/work/
-sudo mkdir ~/maibloom-temp/out/
+sudo mkdir -p /tmp/maibloom-temp/work/
+sudo mkdir -p /tmp/maibloom-temp/out/
 
 # Build command
 sudo mkarchiso -v \
-  -w /home/purple/local-temp/work/ \
-  -o /home/purple/local-temp/out/ .
+  -w /tmp/maibloom-temp/work/ \
+  -o /tmp/maibloom-temp/out/ .
 
 
