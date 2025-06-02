0e7bd08e1a3f3f9d88b82cde5865dc7e103541d9
Amirali Dashti <devoft1@gmail.com>
2025-06-03 00:22:03 +0330

    Update profiledef.sh

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="maibloom-os"
+iso_label="MAIBLOOMOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Mai Bloom Tech Studio <https://maibloom.github.io>"
+iso_application="Mai Bloom os Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
