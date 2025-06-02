cf99f7cdb5214e978b7a1bbcbc9778043e3e6b8c
Amirali Dashti <devoft1@gmail.com>
2025-06-03 00:27:43 +0330

    Update build.sh

--- releng/build.sh
+++ releng/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # changer.sh
 
-WORK_DIR="$HOME/maibloom-temp/work"
-OUT_DIR="$HOME/maibloom-temp/out"
-
-sudo mkdir -p "$WORK_DIR"
-sudo mkdir -p "$OUT_DIR"
-
 sudo mkarchiso -v \
-  -w "$WORK_DIR" \
-  -o "$OUT_DIR" .
+  -w ~/maibloombuild/w/ \
+  -o ~/maibloombuild/o/ .
