30148c06adc98314c75f2cbac2d3bcdbe525e88b
Amirali Dashti <devoft1@gmail.com>
2025-06-03 00:33:32 +0330

    Update build.sh

--- releng/build.sh
+++ releng/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # changer.sh
 
+sudo mkdir -p maibloombuild/w/
+sudo mkdir -p maibloombuild/o/
+
 sudo mkarchiso -v \
-  -w ~/maibloombuild/w/ \
-  -o ~/maibloombuild/o/ .
+  -w maibloombuild/w/ \
+  -o maibloombuild/o/ .
